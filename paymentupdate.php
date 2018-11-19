d3c1cd7467b571d321459e4127c205e25d2babf8
anthgonzalez <45178871+anthgonzalez@users.noreply.github.com>
2018-11-19 15:27:52 -0700

    updated

--- paymentupdate.php
+++ paymentupdate.php
@@ -1,5 +1,3 @@
-<html>
-	<head>
 <link rel="stylesheet" href="styles.css">
 		<title>Buyer Page</title>
 	</head>
@@ -7,16 +5,7 @@
 <body>
 <h1>Outdoor Swap</h1>	
 <a href="Buyer Page.html"><h2>Buyer Information</h2></a>
-	<form>
-<h3>Update Billing Information</h3>	
-			Billing Address:
-			<input type="text" name="BillingAddress"><br>
-			Credit Card #:
-			<input type="text" name="CreditCard"><br>
-			Expiration Date:
-			<input type="text" name="ExpDate"><br>
-			<input type="submit" value="Update">
-		</form>
+	
 </body>
 </html>
 <?php
@@ -29,7 +18,7 @@ if ($conn->connect_error) die("Fatal Error");
 if(isset($_GET['BillingAddress'])){
 	$title = $_GET['BillingAddress'];
 	
-	$query = "select * from movie where BillingAddress = '$BillingAddress' ";
+	$query = "select * from buyer where BillingAddress = '$BillingAddress' ";
 	
 	$result = $conn->query($query);
 	if(!$result) die($conn->error);
@@ -42,15 +31,14 @@ if(isset($_GET['BillingAddress'])){
 		
 		$BillingAddress = $row[1];
 		$CreditCard = $row[2];
-		$ExpDate = $row[3];
+		$Experationdate = $row[3];
 		
 echo <<<_END
-		
+		<pre>
 		<form method='post' action='paymentupdate.php'>
-			<pre>
-				Title: <input type='text' name='BillingAddress' value='$BillingAddress'>
-				Year: <input type='text' name='CreditCard' value='$CreditCard'>
-				Rating: <input type='text' name='ExpDate' value='$ExpDate'>
+				Billing Address: <input type='text' name='BillingAddress' value='$BillingAddress'>
+				Credit Card: <input type='text' name='CreditCard' value='$CreditCard'>
+				Experation Date: <input type='text' name='ExperationDate' value='$ExperationDate'>
 				<input type='submit' value='Update Payment'>
 			</pre>		
 		</form>		
@@ -63,10 +51,10 @@ _END;
 if(isset($_POST['BillingAddress'])){
 	$BillingAddress = $_POST['BillingAddress'];
 	$CreditCard = $_POST['CreditCard'];
-	$ExpDate = $_POST['ExpDate'];
+	$ExperationDate = $_POST['ExperationDate'];
 	
 	
-	$query = "UPDATE move set BillingAddress='$BillingAddress', CreditCard='$CreditCard', ExpDate='$ExpDate' where BillingAddress=$BillingAddress ";
+	$query = "UPDATE buyer set BillingAddress='$BillingAddress', CreditCard='$CreditCard', ExperationDate='$ExperationDate' where BillingAddress=$BillingAddress ";
 	
 	$result = $conn->query($query);
 	if(!$result) die($conn->error);

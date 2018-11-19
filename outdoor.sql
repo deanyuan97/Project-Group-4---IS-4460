21fc512ac13351b6ea92be603f43529cab279f58
cclegg10 <45181564+cclegg10@users.noreply.github.com>
2018-11-19 15:26:13 -0700

    Add files via upload

--- outdoor.sql
+++ outdoor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 19, 2018 at 10:09 PM
+-- Generation Time: Nov 19, 2018 at 10:25 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -28,16 +28,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `buyer` (
   `buyerid` int(11) NOT NULL,
-  `firstname` int(11) DEFAULT NULL,
-  `lastname` int(11) DEFAULT NULL,
-  `buyerusername` int(11) NOT NULL,
-  `buyerpassword` int(11) DEFAULT NULL,
-  `cbuyerpassword` int(11) DEFAULT NULL,
-  `phone` int(11) DEFAULT NULL,
-  `billingaddress` int(11) DEFAULT NULL,
-  `creditcard` int(11) DEFAULT NULL,
-  `experationdate` int(11) DEFAULT NULL
+  `firstname` varchar(11) DEFAULT NULL,
+  `lastname` varchar(11) DEFAULT NULL,
+  `buyerusername` varchar(11) NOT NULL,
+  `buyerpassword` varchar(11) DEFAULT NULL,
+  `cbuyerpassword` varchar(11) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  `billingaddress` varchar(50) DEFAULT NULL,
+  `creditcard` varchar(20) DEFAULT NULL,
+  `experationdate` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `buyer`
+--
+
+INSERT INTO `buyer` (`buyerid`, `firstname`, `lastname`, `buyerusername`, `buyerpassword`, `cbuyerpassword`, `phone`, `billingaddress`, `creditcard`, `experationdate`) VALUES
+(1, 'Cameron', 'Clegg', 'cclegg10', 'test', 'test', '8012440189', '2190 ', 'adfa', 'adfadf');
 
 -- --------------------------------------------------------
 
@@ -96,7 +103,7 @@ ALTER TABLE `seller`
 -- AUTO_INCREMENT for table `buyer`
 --
 ALTER TABLE `buyer`
-  MODIFY `buyerid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `buyerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `item`

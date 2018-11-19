d5f5403ade4e74fc2a6373353f514937325b1fd3
Ben Kulbertis <ben@kulbertis.org>
2018-11-19 15:02:31 -0700

    Added new design to buyerdelete.php

--- buyerdelete.php
+++ buyerdelete.php
@@ -1,20 +1,50 @@
+<!doctype html>
 <html>
-	<head>
-<link rel="stylesheet" href="styles.css">
-		<title>Buyer Page</title>
-	</head>
+<head>
+<meta charset="UTF-8">
+<title>Buyer - Edit</title>
+	<link rel='stylesheet' href="styles.css">
 
-<body>
-<h1>Outdoor Swap</h1>	
-<a href="Buyer Page.html"><h2>Buyer Information</h2><a>
-<form>
-<h3>Delete Account</h3>	
+	<!-- Boot Strap Links -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="styles.css" >
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body style = "background-color: lightgray;">
+
+	<!-- Navbar -->
+	<nav class="navbar navbar-default">
+		<div class="container">
+		<div class="navbar-header">
+			 <a class="navbar-brand" href="#myPage">Outdoor Swap <span class="glyphicon glyphicon-leaf"></span></a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#login">LOGIN</a></li>
+			<li><a href="#listings">LISTINGS</a></li>
+			<li><a href="#services">SERVICES</a></li>
+			<li><a href="#subsciption">SUBSCRIPTION</a></li>
+			<li><a href="#contact">CONTACT</a></li>
+			<li><a href="#about">ABOUT</a></li>
+			</ul>
+		</div>
+		</div>
+	</nav>
+
+	<main class="container-slim text-left">
+
+	<h1><a href="buyerpage.php">Buyer Information</a></h1>
+	<form>
+	<h3>Delete Account</h3>
 			Username:
 			<input type="text" name="Username"><br>
 			Password:
 			<input type="text" name="Password"><br>
 			<input type="submit" value="Delete Account">
-		</form>
+	</form>
 </body>
 </html>
 <?php

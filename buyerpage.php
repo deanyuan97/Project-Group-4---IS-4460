237df9a9bac2985bb1a7baa47203acbbc3c80cbd
Ben Kulbertis <ben@kulbertis.org>
2018-11-19 14:51:21 -0700

    Implemented new design for buyerpage.php.

--- buyerpage.php
+++ buyerpage.php
@@ -1,36 +1,65 @@
+<!doctype html>
 <html>
-	<head>
-<link rel="stylesheet" href="styles.css">
-		<title>Buyer Page</title>
-	</head>
+<head>
+<meta charset="UTF-8">
+<title>Buyer - Add</title>
+	<link rel='stylesheet' href="styles.css">
 
-<body>
-<h1>Outdoor Swap</h1>	
-<h2>Buyer Homepage</h2>
-<p><a href="paymentupdate.html">Update Billing</a> <a href="buyeredit.html">Edit Account Details</a> <a href="buyerdelete.html">Delete Account</a></p>
-	<form>
-<h3>Create Account</h3>	
-			First Name:
-			<input type="text" name="firstname"><br>
-			Last Name:
-			<input type="text" name="lastname"><br>
-			Username:
-			<input type="text" name="Username"><br>
-			Password:
-			<input type="text" name="Password"><br>
-			Confirm Password:
-			<input type="text" name="CPassword"><br>
-			Phone:
-			<input type="text" name="Phone"><br>
-			Billing Address:
-			<input type="text" name="BillingAddress"><br>
-			Credit Card #:
-			<input type="text" name="CreditCard"><br>
-			Expiration Date:
-			<input type="text" name="ExpDate"><br>
-			<input type="submit" value="Create Account">
-		</form>
-<a href="mainpage.html"><h3>Return Home</h3></a>		
+	<!-- Boot Strap Links -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="styles.css" >
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body style = "background-color: lightgray;">
+
+	<!-- Navbar -->
+	<nav class="navbar navbar-default">
+		<div class="container">
+		<div class="navbar-header">
+			 <a class="navbar-brand" href="#myPage">Outdoor Swap <span class="glyphicon glyphicon-leaf"></span></a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#login">LOGIN</a></li>
+			<li><a href="#listings">LISTINGS</a></li>
+			<li><a href="#services">SERVICES</a></li>
+			<li><a href="#subsciption">SUBSCRIPTION</a></li>
+			<li><a href="#contact">CONTACT</a></li>
+			<li><a href="#about">ABOUT</a></li>
+			</ul>
+		</div>
+		</div>
+	</nav>
+
+	<main class="container-slim text-left">
+		<h1>Buyer Homepage</h1>
+		<p><a href="paymentupdate.html">Update Billing</a> | <a href="buyeredit.html">Edit Account Details</a> | <a href="buyerdelete.html">Delete Account</a></p>
+		<form>
+			<h3>Create Account</h3>
+						First Name:
+						<input type="text" name="firstname"><br>
+						Last Name:
+						<input type="text" name="lastname"><br>
+						Username:
+						<input type="text" name="Username"><br>
+						Password:
+						<input type="text" name="Password"><br>
+						Confirm Password:
+						<input type="text" name="CPassword"><br>
+						Phone:
+						<input type="text" name="Phone"><br>
+						Billing Address:
+						<input type="text" name="BillingAddress"><br>
+						Credit Card #:
+						<input type="text" name="CreditCard"><br>
+						Expiration Date:
+						<input type="text" name="ExpDate"><br>
+						<input type="submit" value="Create Account">
+			</form>
+	</main>
 </body>
 </html>
 
@@ -44,7 +73,3 @@ if ($conn->connect_error) die("Fatal Error");
 $conn->close();
 
 ?>
-
-
-
-

91907f5746e5a62b4224c6082b1e9a5b0f43268d
anthgonzalez <45178871+anthgonzalez@users.noreply.github.com>
2018-11-19 15:06:28 -0700

    added database connection and sql queries

--- paymentupdate.php
+++ paymentupdate.php
@@ -26,6 +26,52 @@ $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die("Fatal Error");
 
 
-$conn->close();
+if(isset($_GET['BillingAddress'])){
+	$title = $_GET['BillingAddress'];
+	
+	$query = "select * from movie where BillingAddress = '$BillingAddress' ";
+	
+	$result = $conn->query($query);
+	if(!$result) die($conn->error);
+	
+	$row_count = $result->num_rows;
+
+	for($j=0; $j<$row_count; ++$j){
+		$result->data_seek($j);
+		$row = $result->fetch_array(MYSQLI_NUM);
+		
+		$BillingAddress = $row[1];
+		$CreditCard = $row[2];
+		$ExpDate = $row[3];
+		
+echo <<<_END
+		
+		<form method='post' action='paymentupdate.php'>
+			<pre>
+				Title: <input type='text' name='BillingAddress' value='$BillingAddress'>
+				Year: <input type='text' name='CreditCard' value='$CreditCard'>
+				Rating: <input type='text' name='ExpDate' value='$ExpDate'>
+				<input type='submit' value='Update Payment'>
+			</pre>		
+		</form>		
+_END;
+		
+	}
+	
+}
+
+if(isset($_POST['BillingAddress'])){
+	$BillingAddress = $_POST['BillingAddress'];
+	$CreditCard = $_POST['CreditCard'];
+	$ExpDate = $_POST['ExpDate'];
+	
+	
+	$query = "UPDATE move set BillingAddress='$BillingAddress', CreditCard='$CreditCard', ExpDate='$ExpDate' where BillingAddress=$BillingAddress ";
+	
+	$result = $conn->query($query);
+	if(!$result) die($conn->error);
+	
+}
+
 
 ?>

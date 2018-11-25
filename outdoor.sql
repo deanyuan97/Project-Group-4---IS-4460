cf680db5b704404f156d15c1f8cb93b1400c8ae8
Ben Kulbertis <ben@kulbertis.org>
2018-11-24 21:36:40 -0800

    Minor fixes to schema.

--- outdoor.sql
+++ outdoor.sql
@@ -2,12 +2,14 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Nov 19, 2018 at 10:25 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Host: db
+-- Generation Time: Nov 25, 2018 at 05:36 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,9 +32,8 @@ CREATE TABLE `buyer` (
   `buyerid` int(11) NOT NULL,
   `firstname` varchar(11) DEFAULT NULL,
   `lastname` varchar(11) DEFAULT NULL,
-  `buyerusername` varchar(11) NOT NULL,
-  `buyerpassword` varchar(11) DEFAULT NULL,
-  `cbuyerpassword` varchar(11) DEFAULT NULL,
+  `username` varchar(11) NOT NULL,
+  `password` varchar(11) NOT NULL,
   `phone` varchar(11) DEFAULT NULL,
   `billingaddress` varchar(50) DEFAULT NULL,
   `creditcard` varchar(20) DEFAULT NULL,
@@ -43,8 +44,8 @@ CREATE TABLE `buyer` (
 -- Dumping data for table `buyer`
 --
 
-INSERT INTO `buyer` (`buyerid`, `firstname`, `lastname`, `buyerusername`, `buyerpassword`, `cbuyerpassword`, `phone`, `billingaddress`, `creditcard`, `expirationdate`) VALUES
-(1, 'Cameron', 'Clegg', 'cclegg10', 'test', 'test', '8012440189', '2190 ', 'adfa', 'adfadf');
+INSERT INTO `buyer` (`buyerid`, `firstname`, `lastname`, `username`, `password`, `phone`, `billingaddress`, `creditcard`, `expirationdate`) VALUES
+(1, 'Cameron', 'Clegg', 'cclegg10', 'test', '8012440189', '2190 ', 'adfa', 'adfadf');
 
 -- --------------------------------------------------------
 
@@ -53,23 +54,21 @@ INSERT INTO `buyer` (`buyerid`, `firstname`, `lastname`, `buyerusername`, `buyer
 --
 
 CREATE TABLE `item` (
-  `itemid` int(255) AUTO_INCREMENT NOT NULL,
+  `itemid` int(255) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `price` varchar(50) DEFAULT NULL,
-  `IMG` varchar(255 ) NOT NULL,
-  PRIMARY KEY (`itemid`)
-) ENGINE=InnoDB AUTO_INCREMENT=255 DEFAULT CHARSET=utf8;
+  `IMG` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `item`
+--
 
 INSERT INTO `item` (`itemid`, `name`, `description`, `price`, `IMG`) VALUES
-(1, 'Hydro Flask', 'Large Water Bottle', '$20.00', 'HydroFlask.jpg');
-
-INSERT INTO `item` (`itemid`, `name`, `description`, `price`, `IMG`) VALUES
+(1, 'Hydro Flask', 'Large Water Bottle', '$20.00', 'HydroFlask.jpg'),
 (2, 'Back Pack', 'Hiking Back Pack', '$50.00', 'Backpack.jpg');
 
-INSERT INTO `item` (`itemid`, `name`, `description`, `price`, `IMG`) VALUES
-(3, 'Hiking Shoes', 'Sturdy Shoes for Hiking', '$60.00', 'HikingShoes.jpg');
 -- --------------------------------------------------------
 
 --
@@ -80,8 +79,8 @@ CREATE TABLE `seller` (
   `sellerid` int(11) NOT NULL,
   `firstname` varchar(50) DEFAULT NULL,
   `lastname` varchar(50) DEFAULT NULL,
-  `sellerpassword` varchar(20) DEFAULT NULL,
-  `sellerusername` varchar(20) DEFAULT NULL
+  `password` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -101,32 +100,15 @@ ALTER TABLE `item`
   ADD PRIMARY KEY (`itemid`);
 
 --
--- Indexes for table `seller`
---
-ALTER TABLE `seller`
-  ADD PRIMARY KEY (`sellerid`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `buyer`
---
-ALTER TABLE `buyer`
-  MODIFY `buyerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `itemid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `seller`
---
-ALTER TABLE `seller`
-  MODIFY `sellerid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `itemid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

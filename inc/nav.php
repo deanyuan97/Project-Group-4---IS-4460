cc03a5a89b01d60a7d060ac09b96e7a4a07b99fc
Ben Kulbertis <ben@kulbertis.org>
2018-11-24 20:12:55 -0800

    Reordered and added dividers to nav.

--- inc/nav.php
+++ inc/nav.php
@@ -5,13 +5,15 @@
   </div>
   <div class="collapse navbar-collapse" id="myNavbar">
     <ul class="nav navbar-nav navbar-right">
-    <li><a href="mainpage.php#about">ABOUT</a></li>
-    <li><a href="mainpage.php#events">EVENTS</a></li>
-    <li><a href="loginpage.php">LOGIN</a></li>
-    <li><a href="sellerview.php">SELLER</a></li>
-    <li><a href="buyerpage.php">BUYER</a></li>
-    <li><a href="mainpage.php#pricing">PRICING</a></li>
-    <li><a href="mainpage.php#contact">CONTACT</a></li>
+      <li><a href="mainpage.php#about">ABOUT</a></li>
+      <li><a href="mainpage.php#events">EVENTS</a></li>
+      <li><a href="mainpage.php#pricing">PRICING</a></li>
+      <li><a href="mainpage.php#contact">CONTACT</a></li>
+      <li class="navbar-text">|</li>
+      <li><a href="sellerview.php">SELLER</a></li>
+      <li><a href="buyerpage.php">BUYER</a></li>
+      <li class="navbar-text">|</li>
+      <li><a href="loginpage.php">LOGIN</a></li>
     </ul>
   </div>
   </div>

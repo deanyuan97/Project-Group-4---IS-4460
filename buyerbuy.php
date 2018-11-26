bceeecacef6308b8178017ef2121945780309f3e
Ben Kulbertis <ben@kulbertis.org>
2018-11-25 19:28:19 -0800

    Added missing semicolons.

--- buyerbuy.php
+++ buyerbuy.php
@@ -4,12 +4,12 @@ require_once 'login.php';
 $conn=new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die("Fatal Error");
 
-$itemid=$conn->real_escape_string($_POST['itemid'])
+$itemid=$conn->real_escape_string($_POST['itemid']);
 $userid=$conn->real_escape_string($_SESSION['userid']);
-$query="INSERT INTO sale (item, user) VALUES ($itemid, $userid)"
+$query="INSERT INTO sale (item, user) VALUES ($itemid, $userid)";
 
 if ($conn->query($query)===TRUE){
-	$query="UPDATE item SET sold=1 WHERE id=".$itemid
+	$query="UPDATE item SET sold=1 WHERE id=".$itemid;
 	if ($conn->query($query)===TRUE){
 		header("Location: /buyerorders.php");
 	} else {

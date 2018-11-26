5102a4c3c850448b73302bf77e6192708feace8b
Ben Kulbertis <ben@kulbertis.org>
2018-11-26 09:29:10 -0700

    Updated selleradd

---
+++ processselleradd.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once 'login.php';
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die("Fatal Error");
+
+if(!empty($_POST)){
+  $name = $_POST['name'];
+  $description = $_POST['description'];
+  $price = $_POST['price'];
+  $IMG = $_POST['IMG'];
+
+  $query = "INSERT INTO item (name, description, price, IMG, user) VALUES ('$name', '$description', '$price', '$IMG', {$_SESSION['userid']})";
+
+  $result = $conn->query($query);
+  if (!$result) die($conn->error);
+  else {
+    $conn->close();
+    header("Location: sellerview.php");
+  }
+} else die("WTF");
+?>
--- selleradd.php
+++ selleradd.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Add Post</title>
+<title>Edit Item - Outdoor Swap</title>
 	<link rel='stylesheet' href="styles.css">
 
 	<!-- Boot Strap Links -->
@@ -11,63 +11,19 @@
 	<link rel="stylesheet" href="styles.css" >
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
 </head>
-
 <body style = "background-color: lightgray">
-
-	<!-- Navbar -->
-	<nav class="navbar navbar-default">
-		<div class="container">
-		<div class="navbar-header">
-			 <a class="navbar-brand" href="#myPage">Outdoor Swap <span class="glyphicon glyphicon-leaf"></span></a>
-		</div>
-		<div class="collapse navbar-collapse" id="myNavbar">
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#login">LOGIN</a></li>
-			<li><a href="sellerview.php">LISTINGS</a></li>
-			<li><a href="#services">SERVICES</a></li>
-			<li><a href="#subsciption">SUBSCRIPTION</a></li>
-			<li><a href="#contact">CONTACT</a></li>
-			<li><a href="#about">ABOUT</a></li>
-			</ul>
-		</div>
-		</div>
-	</nav>
-	<div>
-		<form method='post' action='selleradd.php'>
-			<input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
-			<input class="form-control" id="description" name="description" placeholder="Description" type="text" required>
-			<input class="form-control" id="price" name="price" placeholder="Price" type="text" required>
-			<input class="form-control" id="IMG" name="IMG" placeholder="Image" type="text" required>
-			<input type = 'submit' value='Add Post'>
+	<?php include_once 'inc/nav.php' ?>
+	<div class="container-slim text-left bg-white">
+		<form method='post' action='processselleradd.php'>
+				Item Name:<input type='text' name='name'>
+				Description:<input type='text' name='description'>
+				Price:<input type='text' name='price'>
+				Image:<input type='text' name='IMG'>
+				<input type='submit' value='Create'>
 		</form>
+		<div class='clear'></div>
 	</div>
-	</main>
+	<?php include_once 'inc/footer.php' ?>
 </body>
 </html>
-
-<?php
-require_once 'login.php';
-$conn = new mysqli($hn, $un, $pw, $db);
-if ($conn->connect_error) die("Fatal Error");
-
-if (isset($_POST['name']))
-{
-	$name=$_POST['name'];
-	$description=$_POST['description'];
-	$price=$_POST['price'];
-	$IMG=$_POST['IMG'];
-
-	$query="INSERT into item (name, description, price, IMG)
-	values ('$name', '$description', '$price', '$IMG')";
-
-	$result=$conn->query($query);
-	if(!$result) die($conn->error);
-
-}
-
-$conn->close();
-
-
-?>

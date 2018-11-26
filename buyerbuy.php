219bc1db2ec81a265a539b1d8f5f38d3042bebab
Ben Kulbertis <ben@kulbertis.org>
2018-11-25 19:14:52 -0800

    Added buy processor.

---
+++ buyerbuy.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once 'login.php';
+$conn=new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die("Fatal Error");
+
+$itemid=$conn->real_escape_string($_POST['itemid'])
+$userid=$conn->real_escape_string($_SESSION['userid']);
+$query="INSERT INTO sale (item, user) VALUES ($itemid, $userid)"
+
+if ($conn->query($query)===TRUE){
+	$query="UPDATE item SET sold=1 WHERE id=".$itemid
+	if ($conn->query($query)===TRUE){
+		header("Location: /buyerorders.php");
+	} else {
+		die("Error:" .$conn->error);
+	}
+} else {
+	die("Error:" .$conn->error);
+}
+
+$conn->close();
+?>
